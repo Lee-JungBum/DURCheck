657599762ca9f082444416adfb8efe2ccbc63191
jungbum05114@naver.com <jungbum05114@gmail.com>
2022-06-08 14:49:41 +0900

    DB설정 완료

--- src/main/java/iKeeper/medicine_Information/MedicineInformationApplication.java
+++ src/main/java/iKeeper/medicine_Information/MedicineInformationApplication.java
@@ -1,14 +1,16 @@
 package iKeeper.medicine_Information;
 
+import ch.qos.logback.classic.Logger;
+import lombok.extern.slf4j.Slf4j;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 @MapperScan(basePackageClasses = MedicineInformationApplication.class)
 @SpringBootApplication
 public class MedicineInformationApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(MedicineInformationApplication.class, args);
+
 	}
 
 }
